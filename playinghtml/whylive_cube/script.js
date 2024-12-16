29b8ffb80df3538b3a9815e5300001a32e9eb73f
shakayami <fukumurawataru0226@gmail.com>
2024-12-16 23:02:57 +0900

    whylive-cube

---
+++ playinghtml/whylive_cube/script.js
@@ -0,0 +1,11 @@
+﻿const cube = document.querySelector('.cube');
+
+function randomRotate() {
+  const randomX = Math.floor(Math.random() * 360);
+  const randomY = Math.floor(Math.random() * 360);
+  const randomZ = Math.floor(Math.random() * 360);
+
+  cube.style.transform = `rotateX(${randomX}deg) rotateY(${randomY}deg) rotateZ(${randomZ}deg)`;
+}
+
+setInterval(randomRotate, 2000);
